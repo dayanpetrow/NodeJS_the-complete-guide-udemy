c250ec1806dce9af91b5ade85835980f0f0f10e1
Dayan Petrov <dayan.petroff@gmail.com>
2019-05-09 17:54:43 +0300

    Express setup boilerplate

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/admin', adminRoutes); //adds /admin to all adminRoutes
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
