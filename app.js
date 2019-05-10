0e551845086dd202d1c1d1143212bc5270c854a9
Dayan Petrov <dayan.petroff@gmail.com>
2019-05-10 11:29:05 +0300

    Dynamic views with Pug (templating engine)

--- app.js
+++ app.js
@@ -5,17 +5,22 @@ const rootDir = require('./utils/path');
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+app.set('view engine', 'pug');
+app.set('views', 'views'); //no need to do this, it is default directory
+
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes); //adds /admin to all adminRoutes
+app.use('/admin', adminData.routes); //adds /admin to all adminRoutes
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+  res.status(404).render('404', {
+    pageTitle: 'Page not found'
+  });
 });
 
 app.listen(3000, () => {
--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,16 @@ const path = require('path');
 const rootDir = require('../utils/path');
 
 const express = require("express");
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop', {
+    pageTitle: 'Shop',
+    path: '/',
+    prods: adminData.products
+  });
 });
 
 module.exports = router;
